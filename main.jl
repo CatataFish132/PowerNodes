6a734756f1365666b936fff936f4d5b6e6454fc8
Thimo Veldema <thimoveldema@gmail.com>
2022-09-27 08:13:44 +0200

    still needs more work

--- main.jl
+++ main.jl
@@ -12,11 +12,19 @@ include("objects/wind_small/wind_small.jl")
 include("objects/solar/solar.jl")
 include("objects/ecar/ecar.jl")
 include("objects/demand/demand.jl")
+include("smart_objects/battery/battery.jl")
 
 function Calculate()
-    output = sum(values(object_dict))
+    objects = collect(values(object_dict))
+    objects_matrix = mapreduce(permutedims, vcat, objects)
+    output = []
+    for i in eachindex(objects_matrix[1,:])
+        append!(output, sum(objects_matrix[:,i]))
+        output[i] = battery_input(output[i])
+    end
     return output
 end
 
 results = Calculate()
-plot(results)
+# plot(results)
+plot(battery_state_list)
--- smart_objects/battery/battery.jl
+++ smart_objects/battery/battery.jl
@@ -1,29 +1,44 @@
-# for i in 1:8760
-    #     append!(output, sum(values(object_dict)))
+json_settings = open("smart_objects/battery/settings.json")
+settings = Dict()
+settings = JSON.parse(read(json_settings, String))
 
-    #     # Battery Charging Logic
-    #     if output[i] > dict["battery"]["grid charge power"]
-    #         if (dict["battery"]["capacity"] - battery_state) > output[i]
-    #             battery_state += output[i]
-    #             output[i] = 0
-    #         else
-    #             output[i] += battery_state - dict["battery"]["capacity"]
-    #             battery_state += dict["battery"]["capacity"] - battery_state
-    #         end
-    #     end
+battery_state = settings["starting kwh"]
+battery_state_list = []
 
-    #     # Battery Discharging Logic
-    #     if output[i] < dict["battery"]["grid discharge power"]
-    #         diff = output[i]-dict["battery"]["grid discharge power"]
-    #         if abs(diff) < battery_state
-    #             battery_state += diff
-    #             output[i] -= diff
-    #         else
-    #             output[i] - battery_state
-    #             battery_state = 0
-    #         end
-    #     end
-            
+function charge_battery(amount)
+    global battery_state
+    leftover_storage = settings["capacity"] - battery_state
+    if leftover_storage > (amount*settings["charge efficiency"])
+        battery_state += amount*settings["charge efficiency"]
+        return 0
+    else
+        battery_state += leftover_storage
+        return amount - leftover_storage * (1/settings["charge efficiency"])
+    end
+end
 
-    #     append!(battery_state_list, battery_state)
-    # end
+function discharge_battery(amount)
+    global battery_state
+    if battery_state > (-amount * (1/settings["discharge efficiency"]))
+        battery_state += amount * (1/settings["discharge efficiency"])
+        return 0
+    else
+        out = amount + battery_state * settings["discharge efficiency"]
+        battery_state = 0
+        return out 
+    end
+end
+
+function battery_input(input)
+    global battery_state
+    global battery_state_list
+    battery_state -= battery_state * settings["self discharge"]
+    output = input
+    if input > settings["grid charge power"]
+        output = charge_battery(input)
+    elseif input < settings["grid discharge power"]
+        output = discharge_battery(input)
+    end
+    append!(battery_state_list, battery_state)
+    return output
+end
